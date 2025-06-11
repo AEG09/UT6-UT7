01f7fadb5186dbc26ad9a916c185b82067b2f276
Martilerdo <mop290@gmail.com>
2025-06-11 21:34:50 +0100

    Add files via upload
    .

---
+++ src/Main/UserappApplication.java
@@ -0,0 +1,13 @@
+package com.dawm.userapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserappApplication.class, args);
+	}
+
+}
